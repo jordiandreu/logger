c056404e6cca019c9ecc789f315d9d20f259888d
Jordi Andreu <jandreu@cells.es>
2021-11-05 12:15:45 +0100

    Clean up

--- logger/core.py
+++ logger/core.py
@@ -24,15 +24,9 @@ class FactoryLogging:
         not the case, tries to search the configuration file in the default
         logger configuration path.
 
-        If no configuration filename is found, a ValueError exception is
-        raised.
-
         :param filename: configuration file name.
         :return: Absolute path to the configuration file.
         """
-        # if not filename:
-        #     _error = "Configuration filename has not been provided."
-        #     raise ValueError(_error)
         # Check if filename is a valid filepath
         if os.path.isfile(os.path.abspath(filename)):
             return os.path.abspath(filename)
@@ -54,14 +48,14 @@ class FactoryLogging:
     @staticmethod
     def config_from_file(cfg_filename, loglevel="DEBUG"):
         """
-        Logging helper based on a configuration file. This method will always
-        return a valid logger object, following the next steps:
+        Logging helper based on a configuration file. This method will
+        configure a valid logger object, following the next steps:
 
         1) If no configuration file is supplied, a basic default logger is
         provided with a StreamHandler to console.
 
-        2) If a configuration file is supplied, the logger returned is
-        created by using the fileConfig method from the standard python logging
+        2) If a configuration file is supplied, the logger is configured
+        by using the fileConfig method from the standard python logging
         module.
 
         :param cfg_filename: The logging configuration filename.
@@ -70,7 +64,7 @@ class FactoryLogging:
         """
         loglevel = loglevel.upper()
         name = FactoryLogging.__name__
-        # Try to get a valid configuration filepath
+
         try:
             cfg_filepath = FactoryLogging._get_cfg_filepath(cfg_filename)
         except ValueError as e:
@@ -91,5 +85,3 @@ class FactoryLogging:
             logger = logging.getLogger(name)
             logger.setLevel(level[loglevel])
             logger.info("Logging CONFIGURATION from file ({})".format(cfg_filepath))
-
-
